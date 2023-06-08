5852761cd74ff904ca8ee90ce2fad229af2a7a3a
Isichei Chukwuka <chuks89@live.com>
2023-06-08 12:52:33 +0100

    corrections made for challenge-2

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,32 +1,13 @@
 function fizzBuzz(input){
-    if(input % 5 == 0 && input % 3 == 0){
-        return `fizzbuzz`
+    if(input % 3 == 0 && input % 5 == 0){
+        return 'fizzBuzz'
     }else if(input % 3 == 0){
-        return `fizz`
-    }else if (input % 5 == 0){
-        return `buzz`
+        return 'fizz'
+    }else if(input % 5 == 0){
+        return 'buzz'
     }else{
         return input
     }
-  
 }
-function fizzBuzz(input){
-    switch  (input % 5 == 0 && input % 3 == 0){
-    case 1: 
-     return `fizzbuzz`
-    break;   
-    case 2:
-        (input % 3 == 0)
-        return `fizz`
-    break;
-    case 3:(input % 5 == 0)
-        return `buzz`
-        break;
-    default:
-        return input
-    }
-  
-}
-
 
 module.exports = fizzBuzz
